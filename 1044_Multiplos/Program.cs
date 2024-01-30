44a0ab9b18aa09be6cf074ed2815a9520d84dd8f
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2024-01-30 07:48:09 -0300

    primeiro commit

---
+++ 1044_Multiplos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1044_Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+        }
+    }
+}
